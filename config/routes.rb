25e6377d70fcb7df6a4c56d4e8188faf71ef2b96
Maktech07 <maktech07@gmail.com>
2015-05-19 22:05:22 -0500

    Adding of static pages, just creation, not guarded by sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'statics/home'
+
+  get 'statics/help'
+
+  get 'statics/about'
+
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
   devise_for :users
